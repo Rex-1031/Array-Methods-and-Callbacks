5cf1eea8aadd4ec939dc530703e78ca42e83f817
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2020-09-03 23:12:25 -0400

    task 1 completed

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ console.log('its working');
 
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
 
-(a) Home Team name for 2014 world cup final
-(b) Away Team name for 2014 world cup final
-(c) Home Team goals for 2014 world cup final
-(d) Away Team goals for 2014 world cup final
-(e) Winner of 2014 world cup final */
+(a) Home Team name for 2014 world cup final*/
+        console.log(`Task 1a. 2014 World Cup Final Home Team: ${fifaData[828]['Home Team Name']}`)
 
+/*(b) Away Team name for 2014 world cup final*/
+        console.log(`Task 1b. 2014 World Cup Final Away Team: ${fifaData[828]['Away Team Name']}`)
+/*(c) Home Team goals for 2014 world cup final*/
+        console.log(`Task 1c. Home Team Goals: ${fifaData[828]['Home Team Goals']}`)
+/*(d) Away Team goals for 2014 world cup final*/
+        console.log(`Task 1d. Home Team Goals: ${fifaData[828]['Away Team Goals']}`)
+
+/*(e) Winner of 2014 world cup final */
+      console.log(`Task 1e. Winner of the 2014 World Cup Final: ${fifaData[828]['Win conditions']}`)
+
+      
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
